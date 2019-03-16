5db755f8eb9630b637dfadb78efa9c485d86674c
braxalend <alexandrb@inbox.ru>
2019-03-16 16:43:33 +0700

    Решил 5-ю задачу

--- SourceCode/Task5.py
+++ SourceCode/Task5.py
@@ -1,9 +1,12 @@
+#-*- coding: utf-8 -*-
 #2520 - самое маленькое число, которое делится без остатка на все числа от 1 до 10.
 #Какое самое маленькое число делится нацело на все числа от 1 до 20?
 import math
-import time
 import itertools
-start_time = time.time()
+import functools
+
+en = 20
+lp = [2, 3]
 def IsPrime(pc):
     if pc < 2:
         return False
@@ -12,7 +15,14 @@ def IsPrime(pc):
             return False
     return True
 
-l = [i for i in range(5,21) if IsPrime(i)]
+def IsInt(n):
+    return int(n) == float(n)
 
-print (l)
-print("--- %s seconds ---" % (time.time() - start_time))
+ls = [i for i in range(1,en+1)]
+lp.extend([i for i in range(5,en+1) if IsPrime(i)])
+n = {j:max([math.log(i,j) for i in ls if IsInt(math.log(i,j))]) for j in lp}
+print(functools.reduce(lambda x, y: x*y, [x**y for x, y in zip(n.keys(), n.values())]))
+
+print(ls)
+print(lp)
+print(n)
